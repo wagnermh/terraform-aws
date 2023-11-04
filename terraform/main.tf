3530f2030f4c43d38e49648ea7aff4be96eaf2e7
wagnermh <60011914+wagnermh@users.noreply.github.com>
2023-11-03 23:01:36 -0300

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.24.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.nano"
+  tags = {
+    Name = "test_instance"
+  }
+}
