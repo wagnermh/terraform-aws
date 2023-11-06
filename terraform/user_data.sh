4a9368b1eabd1bb0a05f04bd651707e179f55235
wagnermh <60011914+wagnermh@users.noreply.github.com>
2023-11-06 00:29:50 -0300

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt -y update
-sudo apt -y install apache2 git
+sudo apt -y install apache2 git curl
 MYIP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 echo "<h2>WebServer with PrivateIP: $MYIP</h2><br>Built by Terraform external file!" > /var/www/html/index.html
 service apache2 start
