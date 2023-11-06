1c33c047a1f1e19f56cdcc9586ef4eec341d7418
wagnermh <60011914+wagnermh@users.noreply.github.com>
2023-11-05 23:29:46 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,16 @@ provider "aws" {
 resource "aws_instance" "test_instance" {
   ami           = "ami-830c94e3"
   instance_type = "t2.nano"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              #sudo upgrade -y
+              sudo apt-get install -y apache2 php git
+              #sudo git clone https://github.com/brikis98/php-app.git /var/www/html/app
+              sudo service apache2 start
+              EOF
+
   tags = {
     Name = "test_instance"
   }
